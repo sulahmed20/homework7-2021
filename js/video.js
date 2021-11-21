be422d1b78ac1df761ae6c85fb8762bf949b8d31
sulahmed20 <74521422+sulahmed20@users.noreply.github.com>
2021-11-21 13:24:26 -0500

    Added Play/Pause, struggling with volume

--- js/video.js
+++ js/video.js
@@ -1,11 +1,33 @@
-var video;
+var video = document.getElementById("player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+let play = document.getElementById("play");
+let pause = document.getElementById("pause");
+let slower = document.getElementById("slower");
+let faster = document.getElementById("faster");
+let skip = document.getElementById("skip");
+let mute = document.getElementById("mute");
+let slider = document.getElementById("slider");
+let volume = document.getElementById("volume");
+let vintage = document.getElementById("vintage");
+let orig = document.getElementById("orig");
+
+volume.addEventListener("change", function(e) {
+audio.slider = e.currentTarget.value / 100;
+})
+
+play.addEventListener("click", function() {
+	video.play();
+	console.log("Play Video");
+	volume.textContent = video.volume;
+});
+
+pause.addEventListener("click", function() {
+	video.pause();
+	console.log("Pause Video");
+});
 
